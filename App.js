d6db07ea4175633348d58e98325a0760a21656de
Jose Antonio <leao-gomes@hotmail.com>
2022-09-04 22:33:23 -0300

    Finalização da aula

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, View } from 'react-native';
 import Cesta from './src/telas/Cesta';
+import {useFonts , Montserrat_400Regular, Montserrat_700Bold} from "@expo-google-fonts/montserrat";
+import AppLoading from 'expo-app-loading';
+import mock from "./src/mocks/cesta";
+
 
 export default function App() {
+  const [fontCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  });
+
+  if(!fontCarregada) {
+    return <AppLoading />
+  }
+
   return (
-    <SafeAreaView >
+    <SafeAreaView  style={{flex: 1}}>
       <StatusBar />
-      <Cesta />
+      <Cesta {...mock}/>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
